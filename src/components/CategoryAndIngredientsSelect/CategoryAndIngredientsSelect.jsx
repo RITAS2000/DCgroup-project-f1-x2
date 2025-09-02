a5b0306d9cb93ba262db17e6fd61c858f644f339
DmytroValMan <dmitri.m2011@gmail.com>
2025-09-02 16:02:13 +0300

    add fully customized select

--- src/components/CategoryAndIngredientsSelect/CategoryAndIngredientsSelect.jsx
+++ src/components/CategoryAndIngredientsSelect/CategoryAndIngredientsSelect.jsx
@@ -1,6 +1,8 @@
-import { Field, ErrorMessage } from 'formik';
-
+import { ErrorMessage, useField } from 'formik';
+import Select, { components } from 'react-select';
 import css from './CategoryAndIngredientsSelect.module.css';
+
+const SPRITE = '/sprite/symbol-defs.svg';
 
 const CategoryAndIngredientsSelect = ({
   categories,
@@ -8,20 +10,45 @@ const CategoryAndIngredientsSelect = ({
   name,
   id,
 }) => {
+  const [field, meta, helpers] = useField(name);
+
+  const options =
+    categories?.map((category) => ({
+      value: category.name,
+      label: category.name,
+    })) || [];
+
+  const DropdownIndicator = (props) => {
+    const isMenuOpen = props.selectProps.menuIsOpen;
+    return (
+      <components.DropdownIndicator {...props}>
+        <div
+          className={`${css.dropdownIconWrapper} ${isMenuOpen ? css.open : ''}`}
+        >
+          <svg className={css.dropdownIcon} width="24" height="24">
+            <use href={`${SPRITE}#icon-chevron-down`} />
+          </svg>
+        </div>
+      </components.DropdownIndicator>
+    );
+  };
+
   return (
     <div className={css.selectWrapper}>
-      <Field className={css.select} as="select" name={name} id={id}>
-        <option value="" disabled hidden>
-          {placeholder}
-        </option>
-        {Array.isArray(categories) &&
-          categories.map((category) => (
-            <option key={category._id} value={category.name}>
-              {category.name}
-            </option>
-          ))}
-      </Field>
-      <ErrorMessage className={css.errorMsg} name={name} component="span" />
+      <Select
+        inputId={id}
+        name={name}
+        placeholder={placeholder}
+        options={options}
+        value={options.find((opt) => opt.value === field.value) || null}
+        onChange={(selectedOption) =>
+          helpers.setValue(selectedOption?.value || '')
+        }
+        onBlur={() => helpers.setTouched(true)}
+        classNamePrefix="customSelect"
+        components={{ DropdownIndicator }}
+      />
+      <ErrorMessage name={name} component="span" className={css.errorMsg} />
     </div>
   );
 };
